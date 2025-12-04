2020684a6d518f2f995a5459c49e79d913366b4d
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-12-05 02:02:15 +0300

    Enhance ISlidable interface with movement methods
    Added methods to manage sliding state and direction.

--- src/slidingpenguins/objects/ISlidable.java
+++ src/slidingpenguins/objects/ISlidable.java
@@ -1,14 +1,23 @@
 package slidingpenguins.objects;
 
+import slidingpenguins.core.Direction;
+
 /**
  * Interface for objects that have the ability to slide on the icy terrain.
  * Implemented by Penguins and certain Hazards (LightIceBlock, SeaLion).
  */
 public interface ISlidable extends ITerrainObject {
-    
+
     /**
      * Defines the behavior when the object starts sliding.
-     * Once slidable objects start moving, they keep going.
      */
-    void slide(); 
+    void slide();
+
+    boolean isMoving();
+
+    void setMoving(boolean moving);
+
+    Direction getDirection();
+
+    void setDirection(Direction direction);
 }
