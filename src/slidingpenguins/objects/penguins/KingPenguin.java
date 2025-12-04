0c678945f49cef0d5ce993fcad0ec4956f1aa602
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-12-05 02:03:10 +0300

    Extend KingPenguin class from Penguin

--- src/slidingpenguins/objects/penguins/KingPenguin.java
+++ src/slidingpenguins/objects/penguins/KingPenguin.java
@@ -1,4 +1,18 @@
 package slidingpenguins.objects.penguins;
 
-public class KingPenguin {
+public class KingPenguin extends Penguin {
+
+    public KingPenguin(String id) {
+        super(id);
+    }
+
+    @Override
+    public void useSpecialAbility() {
+        if (hasUsedAbility()) {
+            System.out.println(id + " (King) has already used its special ability.");
+            return;
+        }
+        System.out.println(id + " (King) is prepared to stop at the 5th square if needed.");
+        markAbilityUsed();
+    }
 }
