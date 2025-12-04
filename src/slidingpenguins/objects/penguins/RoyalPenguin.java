a754d251810430e912c1ce76c65b49ba60bd8fa1
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-12-05 02:03:55 +0300

    Update RoyalPenguin.java

--- src/slidingpenguins/objects/penguins/RoyalPenguin.java
+++ src/slidingpenguins/objects/penguins/RoyalPenguin.java
@@ -1,4 +1,18 @@
 package slidingpenguins.objects.penguins;
 
-public class RoyalPenguin {
+public class RoyalPenguin extends Penguin {
+
+    public RoyalPenguin(String id) {
+        super(id);
+    }
+
+    @Override
+    public void useSpecialAbility() {
+        if (hasUsedAbility()) {
+            System.out.println(id + " (Royal) has already used its special ability.");
+            return;
+        }
+        System.out.println(id + " (Royal) can move to an adjacent square before sliding.");
+        markAbilityUsed();
+    }
 }
