fc8b625c8dcfd409837113102e05a99838b917d8
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-12-06 15:43:46 +0300

    Add JavaDoc comments to Direction enum
    Added documentation for the Direction enum.

--- src/slidingpenguins/core/Direction.java
+++ src/slidingpenguins/core/Direction.java
@@ -1,8 +1,9 @@
 package slidingpenguins.core;
 
-
-//Represents the four cardinal directions a penguin can move.
- 
+/**
+ * Represents the four cardinal directions a penguin can move.
+ * Used throughout the game to specify movement directions.
+ */
 public enum Direction {
     UP,
     DOWN,
