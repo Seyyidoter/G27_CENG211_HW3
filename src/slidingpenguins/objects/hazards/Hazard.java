94846d18f71f4b1ad255a33a0484319516a8d2ad
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-12-06 15:49:08 +0300

    Refactor Hazard to extend AbstractTerrainObject
    Refactored Hazard class to extend AbstractTerrainObject, removing redundant x and y properties.

--- src/slidingpenguins/objects/hazards/Hazard.java
+++ src/slidingpenguins/objects/hazards/Hazard.java
@@ -1,53 +1,23 @@
 package slidingpenguins.objects.hazards;
 
+import slidingpenguins.objects.AbstractTerrainObject;
 import slidingpenguins.objects.ITerrainObject;
 
 /**
  * Abstract base class for all hazards on the terrain.
- * Implements IHazard marker interface to distinguish from other objects.
+ * Extends AbstractTerrainObject to inherit coordinates.
  */
-public abstract class Hazard implements IHazard {
+public abstract class Hazard extends AbstractTerrainObject implements IHazard {
 
-    protected int x;
-    protected int y;
-
-    // Constructor: Used by subclasses to call super(true/false)
     public Hazard() {
+        super();
     }
 
-    // --- ITerrainObject Implementation ---
+    // x and y are handled by AbstractTerrainObject.
 
-    @Override
-    public int getX() {
-        return x;
-    }
-
-    @Override
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    @Override
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    /**
-     * The symbol of the hazard on the grid (HB, SL, etc.)
-     */
     @Override
     public abstract String getSymbol();
 
-    // --- FIX: Method added ---
-    /**
-     * Abstract method determining what happens when an object collides with this hazard.
-     * All subclasses (HeavyIceBlock, SeaLion, HoleInIce, LightIceBlock) must implement this.
-     * @param incomer The colliding object (usually a Penguin or another sliding object)
-     */
+    @Override
     public abstract void onCollision(ITerrainObject incomer);
 }
