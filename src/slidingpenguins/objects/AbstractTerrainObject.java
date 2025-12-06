85d0f19a9d2a221f9c491ee4e90c084f7d9bb937
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-12-06 13:31:11 +0300

    Add AbstractTerrainObject class for terrain objects
    Introduced an abstract class for terrain objects that centralizes coordinate management.

---
+++ src/slidingpenguins/objects/AbstractTerrainObject.java
@@ -0,0 +1,51 @@
+package slidingpenguins.objects;
+
+/**
+ * Base abstract class for all objects on the terrain.
+ * Centralizes the coordinate logic (x, y) to prevent code duplication.
+ */
+public abstract class AbstractTerrainObject implements ITerrainObject {
+    
+    protected int x;
+    protected int y;
+
+    /**
+     * Default Constructor.
+     */
+    public AbstractTerrainObject() {
+    }
+
+    /**
+     * Copy Constructor.
+     * Used by subclasses to copy coordinates safely.
+     * @param other The object to copy coordinates from.
+     */
+    public AbstractTerrainObject(AbstractTerrainObject other) {
+        if (other != null) {
+            this.x = other.x;
+            this.y = other.y;
+        }
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    // getSymbol() is left for concrete classes or interface default.
+}
